196b0ed6779b62399d3033d056e0cbd64cbdd12f
Augustas-Armalis <armaugustas@gmail.com>
2024-12-27 20:05:16 +0200

    counter cards

---
+++ awebagain/js/dan1.js
@@ -0,0 +1,70 @@
+
+
+// Counter up
+
+const firstCard = 10000;
+const secondCard = 100;
+const thirdCard = 2;
+
+const createOdometer = (el, value) => {
+  const odometer = new Odometer({
+    el: el,
+    value: 0,
+  });
+
+  let hasRun = false;
+
+  const options = {
+    threshold: [0, 0.9],
+  };
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (!hasRun) {
+          odometer.update(value);
+          hasRun = true;
+        }
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  observer.observe(el);
+};
+
+const subscribersOdometer = document.querySelector(".first-odometer");
+createOdometer(subscribersOdometer, firstCard);
+
+const videosOdometer = document.querySelector(".second-odometer");
+createOdometer(videosOdometer, secondCard);
+
+const projectsOdometer = document.querySelector(".third-odometer");
+createOdometer(projectsOdometer, thirdCard);
+
+gsap.registerPlugin(ScrollTrigger);
+
+const counterCards = document.querySelectorAll('.counter-card');
+
+const animateCards = (card, index) => {
+  gsap.fromTo(card,
+    { y: 100, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      ease: "power2.out",
+      delay: isDesktop ? 0.1 * index : 0,
+      scrollTrigger: {
+        trigger: card,
+        start: "top 80%",
+        once: true
+      }
+    });
+};
+
+const isDesktop = window.innerWidth > 768;
+
+counterCards.forEach((card, index) => {
+  animateCards(card, index);
+});
